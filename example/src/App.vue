a506db01ad4221e78e291eb262cd7d97d1f0cc86
Alertis <osmanyavuzdemir96@gmail.com>
2023-02-13 00:45:37 +0300

    create example project

---
+++ example/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+      <VueBarcodes />
+  </div>
+</template>
+
+<script>
+import VueBarcodes from 'vue-barcodes-generator'
+
+export default {
+  name: 'App',
+  components:{
+    VueBarcodes
+  }
+}
+</script>
